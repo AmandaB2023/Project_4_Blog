3baf7326d02852d9ebba5b61b48338b0398ffff6
Amanda Fahy <amandafahy@ymail.com>
2024-07-21 16:36:21 +0000

    Add summernote plugin

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,12 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+# Order posts by newest first.
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -22,3 +28,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+# Order comments by newest first.
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
